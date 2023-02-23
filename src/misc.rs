1a8c634b4b9b4b572e12b5b7f7547556abed2f44
Ye Shengnan <thinkphoebe@gmail.com>
2023-02-23 23:20:55 +0800

    add get_local_ip

--- src/misc.rs
+++ src/misc.rs
@@ -1,9 +1,35 @@
-use anyhow::Context;
+use std::net::UdpSocket;
+
+use anyhow::{anyhow, Context};
 
 pub fn relative2absolute(relative: &str, base: &str) -> anyhow::Result<String> {
     let mut path = std::path::PathBuf::from(base);
     path.pop();
     path.push(relative);
-    path = path.canonicalize() .context(format!("canonicalize {:?}", path.to_str()))?;
+    path = path.canonicalize()
+            .context(format!("canonicalize {:?}", path.to_str()))?;
     Ok(path.to_str().unwrap().to_string())
+}
+
+pub fn get_local_ip() -> anyhow::Result<String> {
+    let socket = match UdpSocket::bind("0.0.0.0:0") {
+        Ok(s) => s,
+        Err(e) => {
+            return Err(anyhow!("socket bind got err:{:?}", e));
+        }
+    };
+
+    // 这里并不需要真正连接成功，只是系统会根据这个 IP 选择对应的网卡，后面获得的也是对应网卡的地址
+    if let Err(e) = socket.connect("8.8.8.8:80") {
+        return Err(anyhow!("socket connect got err:{:?}", e));
+    }
+
+    match socket.local_addr() {
+        Ok(addr) => {
+            Ok(addr.ip().to_string())
+        }
+        Err(e) => {
+            Err(anyhow!("socket local_addr got err:{:?}", e))
+        }
+    }
 }
